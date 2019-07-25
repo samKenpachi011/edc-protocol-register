db398ae6f4bf6efdbacefa9f1eb9664cc3b2a0fd
ckgathi <ckgathi@gmail.com>
2019-07-25 16:30:44 +0200

    fix stucture

--- edc_protocol_register/views/home_view.py
+++ edc_protocol_register/views/home_view.py
@@ -5,3 +5,9 @@ from edc_base.view_mixins import EdcBaseViewMixin
 class HomeView(EdcBaseViewMixin, TemplateView):
 
     template_name = 'edc_protocol_register/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(
+            x='test value')
+        return context
