a68e30042ac33b9ee6cf7b72481372d4e3031e6a
salahdin zeberga <salahdinzeberga@admins-MacBook-Air-7.local>
2019-07-26 12:26:02 +0200

    requesting protocol works

--- edc_protocol_register/views/request_view.py
+++ edc_protocol_register/views/request_view.py
@@ -1,17 +1,21 @@
 from django.views.generic import CreateView
 from edc_base.view_mixins import EdcBaseViewMixin
 from ..models import ProtocolRequest
+from ..forms import ProtocolRequestForm
 from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
+from django.utils import timezone
 
 
 class RequestView(EdcBaseViewMixin, CreateView):
 
     template_name = 'edc_protocol_register/apply.html'
     model = ProtocolRequest
-    fields = ['name', 'description', 'email', 'pi_email', 'duration']
+    form_class = ProtocolRequestForm
+    success_url = reverse_lazy('home_url')
 
     def form_valid(self, form):
-        request = super(RequestView, self).save(commit=False)
-        self.request.save()
+        self.object = form.save(commit=False)
+        self.object.request_date = timezone.now()
+        self.object.save()
         return HttpResponseRedirect(self.get_success_url())
