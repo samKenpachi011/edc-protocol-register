5b918817bc853c1f48b62415ac86930d70b25f1e
salahdin zeberga <salahdinzeberga@admins-MacBook-Air-7.local>
2019-08-02 09:28:19 +0200

    database removed from gitignore

--- edc_protocol_register/import_data.py
+++ edc_protocol_register/import_data.py
@@ -1,11 +1,21 @@
 from .models import ProtocolRequest
 import csv
+from django.utils.timezone import datetime
+from .approvalManager import ApproveProtocol
 
+ap = ApproveProtocol()
 
-def read_and_create():
-    with open("") as f:
+def read_and_create_request():
+    with open("BHP_Studies.csv") as f:
         reader = csv.reader(f)
         for row in reader:
-            ProtocolRequest.objects.create(
-                name
+            P_request = ProtocolRequest.objects.create(
+                name=row[0],
+                description=row[1],
+                email='default@default.com',
+                pi_email='default@default.com',
+                request_date=datetime.now(),
             )
+
+            ap.approve(P_request)
+
