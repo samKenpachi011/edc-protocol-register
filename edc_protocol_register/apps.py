d5b4072be2123c4fd8a89cd5511f9dfbac41410c
COulson Kgathi <ckgathi@gmail.com>
2020-04-17 17:18:14 +0200

    add data tables static files

--- edc_protocol_register/apps.py
+++ edc_protocol_register/apps.py
@@ -5,9 +5,10 @@ from edc_device.constants import CENTRAL_SERVER
 #from .models import protocol,protocol_response,protocol_request
 
 
-
 class AppConfig(DjangoAppConfig):
     name = 'edc_protocol_register'
+    verbose_name = "EDC Protocol Register"
+    admin_site_name = 'edc_protocol_register_admin'
 
     def ready(self):
         from .models import protocol_request_on_post_save
