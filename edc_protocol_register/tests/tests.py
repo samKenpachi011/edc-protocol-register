aee9b08263358ce9450af0f23af0c82217b6f4f7
samKenpachi011 <samuelkabelo1@gmail.com>
2023-09-15 11:21:23 +0200

    UPDATE: requirements

--- edc_protocol_register/tests/tests.py
+++ edc_protocol_register/tests/tests.py
@@ -33,19 +33,24 @@ class ProtocolRequestTest(TestCase):
                   'request_date': '2019-02-02'})
         self.assertTrue(form.is_valid())
 
-#     # Invalid Form Data
-#     def test_ProtocolRequestForm_invalid(self):
-#         form = protocol_request_form(data={'short_name': "", 'long_name':'', 'description': "sadfasdfasdf", 'email': "salah@gmail.com",
-#                                          'pi_email': "salah@gmail.com", 'request_date': 'A'})
-#         self.assertFalse(form.is_valid())
+    # Invalid Form Data
+    def test_ProtocolRequestForm_invalid(self):
+        form = ProtocolRequestForm(
+            data={'short_name': "",
+                  'long_name': '',
+                  'description': "sadfasdfasdf",
+                  'email': "salah@gmail.com",
+                  'pi_email': "salah@gmail.com",
+                  'request_date': 'A'})
+        self.assertFalse(form.is_valid())
 
-#     # testing email validator
-#     def test_ProtocolRequestForm_with_invalid_email(self):
-#         form = protocol_request_form(
-#             data={'short_name': "qwert",
-#                   'description': "sadfasdfasdf",
-#                   'pi_email': "salah"})
-#         self.assertFalse(form.is_valid())
+    # testing email validator
+    def test_ProtocolRequestForm_with_invalid_email(self):
+        form = ProtocolRequestForm(
+            data={'short_name': "qwert",
+                  'description': "sadfasdfasdf",
+                  'pi_email': "salah"})
+        self.assertFalse(form.is_valid())
 
 
 # class ProtocolApprovalTest(TestCase):
